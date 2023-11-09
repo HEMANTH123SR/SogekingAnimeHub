7757765dca5354e47289df05268839edbf6efcd8
HEMANTH123SR <applicationcomputer257@gmail.com>
2023-11-09 20:36:15 +0530

    react config

---
+++ soge-king-blog-web-app/src/App.jsx
@@ -0,0 +1,11 @@
+import Navigation from "./components/Navigation/Navigation"
+
+const App=()=>{
+  return(
+    <div >
+      <Navigation/>
+    </div>
+  )
+}
+
+export default App;
---
+++ soge-king-blog-web-app/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+import { GiHamburgerMenu } from "react-icons/gi";
+import { BiSearch } from "react-icons/bi";
+import logo from "../../assets/soge-king-logo2.jpg";
+import logo2 from "../../assets/soge-king-logo.jpg";
+
+const Navigation = () => {
+  return (
+    <div className="bg-[#242428]">
+   
+    </div>
+  );
+};
+
+export default Navigation;
