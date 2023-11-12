30f45f415146ba038c86aa86912c17f89e75fcd9
HEMANTH123SR <applicationcomputer257@gmail.com>
2023-11-12 08:29:19 +0530

    build

--- soge-king-blog-web-app/src/components/Navigation.jsx
+++ soge-king-blog-web-app/src/components/Navigation.jsx
@@ -2,7 +2,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { handleLoginBtnCliked } from "../store/AuthReducer";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { BiSearch } from "react-icons/bi";
-import logo from "../assets/soge-king-logo.jpg";
 import usoopGuest from "../assets/guest/usoop-guest-3.jpg";
 import { useState } from "react";
 
@@ -10,12 +9,12 @@ const Navigation = () => {
   const dispatch = useDispatch();
   const authState = useSelector((state) => state.auth);
   const [searchBtnCliked, setSearchBtnClicked] = useState(false);
+
   return (
     <div className="bg-[#242428] relative shadow-lg">
       <div className="flex p-5 ">
         <div className="flex items-center w-2/5 space-x-4 sm:space-x-5 lg:space-x-6 xl:space-x-7">
           <GiHamburgerMenu className="text-4xl text-[#FFDD95]" />
-          <img src={logo} alt="" className="w-10 h-10 rounded-full" />
         </div>
         <div className="w-3/5 flex justify-center">
           <h1 className="text-2xl text-[#FFDD95] font-semibold">
