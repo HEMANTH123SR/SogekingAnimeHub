fd255f88a5074e2396a02c7564ebdda852077353
HEMANTH123SR <applicationcomputer257@gmail.com>
2023-11-12 08:07:45 +0530

    refactor :: Tags.jsx :: added responsiveness & modularized the code

--- soge-king-blog-web-app/src/components/Tags.jsx
+++ soge-king-blog-web-app/src/components/Tags.jsx
@@ -1,72 +1,36 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 const Tags = () => {
   const pathName = useLocation().pathname;
   const [location, setLocation] = useState("/");
+
   useEffect(() => {
     setLocation(pathName);
   }, [pathName]);
+
   return (
-    <div className="flex justify-center m-4  text-[#FFFFFF] font-semibold ">
-      <div className="flex m-4  ">
-        <div
-          className={`  text-lg border-b-2  ${
-            location == "/" ? "text-[#FFDD95]" : ""
-          }`}
-          style={{ borderColor: `${location == "/" ? "#FFDD95" : "#FFFFFF"}` }}
-        >
-          <Link to="/" className="px-4">
-            Popular
-          </Link>
-        </div>
-
-        <div
-          className={`  text-lg border-b-2  ${
-            location == "/recent" ? "text-[#FFDD95]" : ""
-          }`}
-          style={{ borderColor: `${location == "/recent" ? "#FFDD95" : "#FFFFFF"}` }}
-        >
-          <Link to="/recent" className="px-4">
-            Recent
-          </Link>
-        </div>
-
-        <div
-          className={`  text-lg border-b-2  ${
-            location == "/bigthree" ? "text-[#FFDD95]" : ""
-          }`}
-          style={{ borderColor: `${location == "/bigthree" ? "#FFDD95" : "#FFFFFF"}` }}
-        >
-          <Link to="/bigthree" className="px-4">
-            Big Three
-          </Link>
-        </div>
-
-        <div
-          className={`  text-lg border-b-2  ${
-            location == "/ninenovas" ? "text-[#FFDD95]" : ""
-          }`}
-          style={{ borderColor: `${location == "/ninenovas" ? "#FFDD95" : "#FFFFFF"}` }}
-        >
-          <Link to="/ninenovas" className="px-4">
-            Nine Nova
-          </Link>
-        </div>
-
-        <div
-          className={`  text-lg border-b-2  ${
-            location == "/favourite" ? "text-[#FFDD95]" : ""
-          }`}
-          style={{ borderColor: `${location == "/favourite" ? "#FFDD95" : "#FFFFFF"}` }}
-        >
-          <Link to="/favourite" className="px-4">
-            Favourite
-          </Link>
-        </div>
+    <div className="flex justify-center text-[#FFFFFF] font-semibold overflow-x-auto">
+      <div className="flex m-4 space-x-4">
+        <TagLink to="/" currentPath={location}>Popular</TagLink>
+        <TagLink to="/recent" currentPath={location}>Recent</TagLink>
+        <TagLink to="/bigthree" currentPath={location}>Big Three</TagLink>
+     
+        <TagLink to="/favourite" currentPath={location}>Favourite</TagLink>
       </div>
     </div>
   );
 };
+
+const TagLink = ({ to, currentPath, children }) => (
+  <div
+    className={`md:text-base lg:text-base border-b-2 ${currentPath === to ? "text-[#FFDD95]" : ""}`}
+    style={{ borderColor: `${currentPath === to ? "#FFDD95" : "#FFFFFF"}` }}
+  >
+    <Link to={to} className="px-1 sm:px-4 md:px-5 lg:px-6">
+      {children}
+    </Link>
+  </div>
+);
 
 export default Tags;
 
